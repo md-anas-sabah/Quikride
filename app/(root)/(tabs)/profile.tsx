b734c88625e6f24a56470e4e258fae2e5c70618d
md-anas-sabah <mdanassabah@gmail.com>
2024-09-08 00:15:21 +0530

    ADD: Auth section

---
+++ app/(root)/(tabs)/profile.tsx
@@ -0,0 +1,12 @@
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const Profile = () => {
+  return (
+    <SafeAreaView>
+      <Text>Profile from tab</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Profile;
